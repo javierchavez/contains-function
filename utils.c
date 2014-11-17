c879df643d4b14b55bfc05e8d399f3be48e1073a
Javier C <youracow@gmail.com>
2014-11-16 18:54:42 -0700

    Update utils.c

--- utils.c
+++ utils.c
@@ -1,14 +1,11 @@
-/*
-    Javier C
- */
+/* Javier C */
 
 /*
  This is a contains opperation. Checks to see if one string contains another.
  returns postion of first char if found
  returns -1 if not found
  */
-int findString(char srcStr[], char toFind[])
-{
+int findString(char srcStr[], char toFind[]) {
     int y = 0, x = 0;
 
     while (srcStr[x] && toFind[y]) {
@@ -22,5 +19,4 @@ int findString(char srcStr[], char toFind[])
     if (toFind[y]) { return -1; }
     
     return x-y;
-    
 }
